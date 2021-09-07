c793708b5678bb858d2e5a7125625982c30ae4a6
Jordan Freeman <jordan.f670@stu.nebo.edu>
2021-09-07 09:19:49 -0600

    added tank physics

--- Prototype 1-Tank Sim/Assets/Scripts/PlayerController.cs
+++ Prototype 1-Tank Sim/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float turnSpeed;
+    //left and right input
+    public float hInput;
+    //forward and back input
+    public float vInput;
     // projectile
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        // Makes the vehicle go left and right
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * hInput);
+        //makes the vehicle go forward and back
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+        
     }
 }
